c8cdbe51cca897d6835c99884af5182b803269f9
Daniel <danieldunc@hotmail.com>
2020-10-26 18:19:33 +1100

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from ibapi.client import EClient
+from ibapi.wrapper import EWrapper
+
+
+class IBapi(EWrapper, EClient):
+    def __init__(self):
+        EClient.__init__(self, self)
+
+
+app = IBapi()
+app.connect('127.0.0.1', 4002, 42)
+app.run()
+
+'''
+#Uncomment this section if unable to connect
+#and to prevent errors on a reconnect
+import time
+time.sleep(2)
+app.disconnect()
+'''
