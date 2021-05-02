47c1b031d32032a09675e91f7d83abcc8b9fe340
Daniel Duncan <danieldunc@hotmail.com>
2021-05-02 21:58:04 +1000

    Changed server threads from 1 to 8.

--- code/display.py
+++ code/display.py
@@ -28,4 +28,4 @@ def connectivity():
     import connectivitytest
     return "Check console for connection status."
 
-# serve(app, host='0.0.0.0', port=8080, threads=1) # waitress
+# serve(app, host='0.0.0.0', port=8080, threads=8) # waitress
